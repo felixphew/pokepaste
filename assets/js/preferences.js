9c406f6ad588084713f11138bbce19e51e303e7e
Felix Friedlander <felixphew0@gmail.com>
2018-07-19 19:09:40 +1000

    Implement paste options
    Much cleaner than old version.
    
    Accesses cookies in a slightly less dodgy way (still kind of dodgy).
    
    (Visual) CSS still subject to change.

---
+++ assets/js/preferences.js
@@ -0,0 +1,29 @@
+const makeListener = (tgt, id, href) => {
+	tgt.addEventListener("change", e => {
+		const checked = e.currentTarget.checked;
+		const cssid = id + "css";
+		let css = document.getElementById(cssid);
+		if (checked) {
+			if (!css) {
+				css = document.createElement("link");
+				css.id = cssid;
+				css.rel = "stylesheet";
+				css.href = href;
+				document.head.appendChild(css);
+			}
+		} else {
+			if (css) {
+				css.parentNode.removeChild(css);
+			}
+		}
+		document.cookie = `${id}=${checked}`;
+	});
+
+	tgt.checked = false;
+
+	if (document.cookie.includes(`${id}=true`)) tgt.click();
+};
+	
+makeListener(document.getElementById("vgcmode"), "vgc", "/css/paste-vgc.css");
+makeListener(document.getElementById("evivmode"), "eviv", "/css/syntax-eviv.css");
+makeListener(document.getElementById("lightmode"), "light", "/css/paste-light.css");
