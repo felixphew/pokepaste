aaa2cf9fc797c00dea35a01162ed92488a046bc6
Felix Friedlander <felixphew0@gmail.com>
2018-07-29 18:42:41 +1000

    Add provisional HSTS and CSP

--- server.go
+++ server.go
@@ -46,6 +46,9 @@ func servePaste(w http.ResponseWriter, id uint64, p string) {
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Strict-Transport-Security", "max-age=3600")
+	w.Header().Set("Content-Security-Policy", "default-src 'self'")
+
 	if m := pathNew.FindStringSubmatch(r.URL.Path); m != nil {
 		src, err := hex.DecodeString(m[1])
 		if err != nil {
